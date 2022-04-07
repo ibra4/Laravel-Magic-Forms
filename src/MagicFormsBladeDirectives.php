2c1c92843f01ed2316534b20565393beb1436f97
ibra <i.hammad@syarah.com>
2022-04-07 20:32:35 +0300

    No need for type check in blade directives

--- src/MagicFormsBladeDirectives.php
+++ src/MagicFormsBladeDirectives.php
@@ -18,9 +18,6 @@ class MagicFormsBladeDirectives
 
     private static function loadAsset($type)
     {
-        if (!file_exists("vendor/magicforms/$type")) {
-            throw new Exception("You must publish assstes first");
-        }
         $src = asset("vendor/magicforms/$type/magicforms.$type");
         if ($type == 'css') {
             return "<link rel='stylesheet' href='$src'></link>";
